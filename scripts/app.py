875c8cf47e8c0eef48898877e7661003eddf95d5
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-03-06 21:53:43 +0100

    loader

--- scripts/app.py
+++ scripts/app.py
@@ -1,8 +1,18 @@
+from magicgui import magicgui
+import napari
 import cortical
 
 
-filepath = cortical.open_file_dialog()
-print(filepath)
-data = cortical.load_openfus_mat(filename=filepath)
-print(data)
 
+
+viewer = napari.Viewer()
+
+@magicgui(call_button="Load Fusion File")
+def load_fusion_file():
+    filepath = cortical.open_file_dialog()
+    print(filepath)
+    data = cortical.load_openfus_mat(filename=filepath)
+    viewer.add_image(data.image, name='Fusion')
+viewer.window.add_dock_widget(load_fusion_file)
+
+napari.run()
