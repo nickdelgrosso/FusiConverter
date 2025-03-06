8566648adfc969b357f8d6e74290da1ffdf0c5d6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-03-06 22:03:36 +0100

    wrap

--- scripts/app.py
+++ scripts/app.py
@@ -1,18 +1,27 @@
+from dataclasses import dataclass
+from typing import Callable
 from magicgui import magicgui
 import napari
 import cortical
 
 
+@dataclass
+class LoadFusionFile:
+    _viewer: napari.Viewer
+    _open_dialog: Callable[[], str] = cortical.open_file_dialog
+    _loader: Callable[[str], cortical.OpenfusFusionData] = cortical.load_openfus_mat
+
+    def __call__(self):
+        filepath = self._open_dialog()
+        print(filepath)
+        data = self._loader(filename=filepath)
+        self._viewer.add_image(data.image, name='Fusion')
+
 
 
 viewer = napari.Viewer()
 
-@magicgui(call_button="Load Fusion File")
-def load_fusion_file():
-    filepath = cortical.open_file_dialog()
-    print(filepath)
-    data = cortical.load_openfus_mat(filename=filepath)
-    viewer.add_image(data.image, name='Fusion')
+load_fusion_file = magicgui(call_button="Load Fusion File")(LoadFusionFile(_viewer=viewer))
 viewer.window.add_dock_widget(load_fusion_file)
 
 napari.run()
